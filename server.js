cfde513d977f135012d52acd168656596b4b5840
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-01-01 04:53:17 +0300

    v1.0.7

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const users = require("./routes/user.route");
 const messages = require("./routes/message.route")
 const contacts = require("./routes/contacts.route")
-const group_address = require("./routes/group.route")
+const group_address=require("./routes/group.route")
 
 
 
@@ -14,11 +14,15 @@ const group_address = require("./routes/group.route")
 const app = express();
 // middleware
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
 app.use(cors());
 
 // routes
-app.get('/', (req, res) => res.json("api"))
+app.get('/',(req, res)=>res.json("api") )
 app.use("/api/users", users);
 app.use("/api/messages", messages);
 app.use("/api/contacts", contacts);
